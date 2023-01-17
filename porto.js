72a24022c43ca589f1100e8b19475e930aa835ef
Tari <ummu.fasa1886@gmail.com>
2023-01-17 12:43:35 +0700

    membuat emailjs

--- porto.js
+++ porto.js
@@ -10,3 +10,25 @@ document.addEventListener("click", function (e) {
     header.classList.remove("active");
   }
 });
+
+function sendMail() {
+  const pesan = {
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
+  };
+
+  const serviceID = "service_7tyysrk";
+  const templateID = "template_2i52mdm";
+
+  emailjs
+    .send(serviceID, templateID, pesan)
+    .then((res) => {
+      document.querySelector("#name").value = "";
+      document.querySelector("#email").value = "";
+      document.querySelector("#message").value = "";
+      console.log(res);
+      alert("Pesan Kamu telah Terkirim.");
+    })
+    .catch((err) => console.log(err));
+}
