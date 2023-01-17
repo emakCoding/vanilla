4744ae1de201ec89b4d488bc5fef9685274b97a3
Tari <ummu.fasa1886@gmail.com>
2023-01-17 11:44:33 +0700

    perubahan

--- porto.js
+++ porto.js
@@ -1,2 +1,12 @@
-const aktif = document.querySelectorAll(".innermenu a");
-aktif.classList.add(active);
+const header = document.querySelector(".header");
+document.querySelector(".tombol").onclick = () => {
+  header.classList.toggle("active");
+};
+
+const tombol = document.querySelector(".tombol");
+
+document.addEventListener("click", function (e) {
+  if (!tombol.contains(e.target) && !header.contains(e.target)) {
+    header.classList.remove("active");
+  }
+});
